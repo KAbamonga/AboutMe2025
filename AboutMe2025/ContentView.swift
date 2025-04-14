5cb6fdeb8475ef9137244b1b5bfd8adb4a53fdf5
Kiara Eunice Abamonga <kiara.abamonga@gmail.com>
2025-04-13 23:42:54 -0400

    Finished About me App, Iteration 3

--- AboutMe2025/ContentView.swift
+++ AboutMe2025/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var photo = "aasu"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            
+            Color("Cyan").ignoresSafeArea()
+            
+            VStack(alignment: .leading, spacing: 20.0) {
+                Image(photo)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(15)
+                
+                HStack(spacing: 20.0) {
+                    Text("Kiara Abamonga")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                    
+                    Text("🤭🕺🫶🏽😂😜😙")
+                }
+                
+                Text("I’m passionate about creating spaces where students feel seen, heard, and celebrated — especially within the APIDA community. Through AASU and FCA, I advocate for inclusive, cultural spaces where we can thrive together. The photo above is me and my AASU family!")
+                
+                
+                Button("Click me for a FCA photo!") {
+                    photo = "fca"
+                }
+                .font(.title2)
+                .buttonStyle(.borderedProminent)
+                .tint(Color("Cyan"))
+            }
+            
+            .padding()
+            .background(Rectangle() .foregroundColor(Color("Snow")))
+            .cornerRadius(15)
+            .shadow(radius: 15)
+            .padding()
         }
-        .padding()
     }
 }
 
