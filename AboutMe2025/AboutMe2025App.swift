ef03fe0f31eb26eda1f37bcc883e744388618f50
Kiara Eunice Abamonga <kiara.abamonga@gmail.com>
2025-04-13 23:10:12 -0400

    Initial Commit

---
+++ AboutMe2025/AboutMe2025App.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMe2025App.swift
+//  AboutMe2025
+//
+//  Created by Kiara on 4/13/25.
+//
+
+import SwiftUI
+
+@main
+struct AboutMe2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
